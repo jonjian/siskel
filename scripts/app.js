ebf23637782b3ac7b95098cf081408a3e2844a9d
Jonathan Jian <30997582+jonjian@users.noreply.github.com>
2017-12-11 15:46:06 -0800

    finish siskel

---
+++ scripts/app.js
@@ -0,0 +1,97 @@
+var Movie = Backbone.Model.extend({
+
+  defaults: {
+    like: true
+  },
+
+  toggleLike: function() {
+    return !this.get('like') ? this.set('like', true) : this.set('like', false);
+  }
+
+});
+
+var Movies = Backbone.Collection.extend({
+
+  model: Movie,
+
+  initialize: function() {
+    this.on('change', this.sort, this);
+    // this.sortByField(this.comparator);
+  },
+
+  comparator: 'title',
+
+  sortByField: function(field) {
+    this.comparator = field;
+    this.sort(this.comparator);
+  }
+
+});
+
+var AppView = Backbone.View.extend({
+
+  events: {
+    'click form input': 'handleClick'
+  },
+
+  handleClick: function(e) {
+    var field = $(e.target).val();
+    this.collection.sortByField(field);
+  },
+
+  render: function() {
+    new MoviesView({
+      el: this.$('#movies'),
+      collection: this.collection
+    }).render();
+  }
+
+});
+
+var MovieView = Backbone.View.extend({
+
+  template: _.template('<div class="movie"> \
+                          <div class="like"> \
+                            <button><img src="images/<%- like ? \'up\' : \'down\' %>.jpg"></button> \
+                          </div> \
+                          <span class="title"><%- title %></span> \
+                          <span class="year">(<%- year %>)</span> \
+                          <div class="rating">Fan rating: <%- rating %> of 10</div> \
+                        </div>'),
+
+  initialize: function() {
+    this.model.on('change', this.render, this);
+  },
+
+  events: {
+    'click button': 'handleClick'
+  },
+
+  handleClick: function() {
+    this.model.toggleLike();
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
+  }
+
+});
+
+var MoviesView = Backbone.View.extend({
+
+  initialize: function() {
+    this.collection.on('sort', this.render, this);
+  },
+
+  render: function() {
+    this.$el.empty();
+    this.collection.forEach(this.renderMovie, this);
+  },
+
+  renderMovie: function(movie) {
+    var movieView = new MovieView({model: movie});
+    this.$el.append(movieView.render());
+  }
+
+});
